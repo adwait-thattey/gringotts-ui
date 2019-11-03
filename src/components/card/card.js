dde8a78b06df7eaeaed3155126f42fdb554ba730
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-11-03 10:06:27 +0530

    REFACTOR: remove sidenav

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import classes from './style.module.css';
+import { Row, Col } from 'react-materialize';
 
 const Card = ({ text, left, right }) => {
-    return (  
-        <div 
+    return (
+        <div
             className={classes.cardWrapper}
-            style={{ background: `linear-gradient(to top, ${left}, ${right})` }}
+            style={{ background: `${right}` }}
         >
-            <div className={classes.text}>
-                <h6 className={classes.heading}>{text}</h6>
-            </div>
+            <Row>
+                <Col l={6}>
+                    <div style={{ margin: '20px' }}>
+                        <h6 className={classes.heading}>{text}</h6>
+                        <p className={classes.subinfo}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        </p>
+                    </div>
+                </Col>
+                <Col l={6}>
+                    <div className={classes.imgDiv}>
+                        <img className={classes.img} src="https://www.besttechie.com/wp-content/uploads/2018/01/top-5-cloud-service-providers.png"></img>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 }
- 
+
 export default Card;
--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -4,61 +4,54 @@ import Card from '../../components/card/card';
 import '../../App.scss';
 
 class Dashboard extends Component {
-    state = {  
+    state = {
 
     }
 
-    render() { 
-        return (  
+    render() {
+        return (
             <section>
                 <Row>
-                    <Col l={4}>
-                        <h1>Hi</h1>
+                    <Col l={6}>
+                        <div className="card-wrapper">
+                            <Card
+                                text="Credentials"
+                                left="#7c69f4"
+                                right="#5546f2"
+                            />
+                        </div>
                     </Col>
-                    <Col l={8}>
-                        <Row>
-                            <Col l={6}>
-                                <div className="card-wrapper">
-                                    <Card 
-                                        text="Credentials"
-                                        left="#7c69f4"
-                                        right="#5546f2"
-                                    />
-                                </div>
-                            </Col>
-                            <Col l={6}>
-                                <div className="card-wrapper">
-                                    <Card 
-                                        text="Dynamic Credentials"
-                                        left="#ffb347"
-                                        right="#ffcc33"
-                                    />
-                                </div>
-                            </Col>
-                            <Col l={6}>
-                                <div className="card-wrapper">
-                                    <Card 
-                                        text="SSH"
-                                        left="#83a4d4"
-                                        right="#b6fbff"
-                                    />
-                                </div>
-                            </Col>
-                            <Col l={6}>
-                                <div className="card-wrapper">
-                                    <Card
-                                        text="GPG"
-                                        left="#870000"
-                                        right="#190a05"
-                                    />
-                                </div>
-                            </Col>
-                        </Row>
+                    <Col l={6}>
+                        <div className="card-wrapper">
+                            <Card
+                                text="Dynamic Credentials"
+                                left="#ffb347"
+                                right="#ffcc33"
+                            />
+                        </div>
+                    </Col>
+                    <Col l={6}>
+                        <div className="card-wrapper">
+                            <Card
+                                text="SSH"
+                                left="#83a4d4"
+                                right="#b6fbff"
+                            />
+                        </div>
+                    </Col>
+                    <Col l={6}>
+                        <div className="card-wrapper">
+                            <Card
+                                text="GPG"
+                                left="#870000"
+                                right="#190a05"
+                            />
+                        </div>
                     </Col>
                 </Row>
             </section>
         );
     }
 }
- 
+
 export default Dashboard;
