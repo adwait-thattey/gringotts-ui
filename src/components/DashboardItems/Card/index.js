81aaf847bf05ef136321cc1ffdb629646b9d81a8
BrijeshBumrela <brijeshbumrela@gmail.com>
2019-11-15 19:47:44 +0530

    FEAT: category card complete

--- src/components/DashboardItems/Card/index.js
+++ src/components/DashboardItems/Card/index.js
@@ -4,10 +4,15 @@ import classes from './style.module.css';
 const Card = ({ categoryName, credList }) => {
     return (  
         <div className={classes.card}>
-            <h3>{categoryName}</h3>
+            <div className={classes.heading}>
+                <h3>{categoryName}</h3>
+                <h4 className={classes.faintText}>Credentials List</h4>
+            </div>
             <ul>
                 {credList.map((cred, index) => (
-                    <li key={index}>{cred}</li>
+                    <li key={index} className={`colorObj${index % 3}`}>
+                        <span>{cred}</span>
+                    </li>
                 ))}
             </ul>
         </div>
